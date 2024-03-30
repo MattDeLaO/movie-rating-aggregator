3df8fa86c70c20a96e3d55e6b11c574ec06f16b6
Matt De La O <metasploitmatt@gmail.com>
2024-03-29 22:19:23 -0500

    add: loading state for streaming availability and some error handling

--- src/components/StreamingAvailability.tsx
+++ src/components/StreamingAvailability.tsx
@@ -8,8 +8,8 @@ import {
   TableContainer,
   TableHead,
   TableRow,
-  Paper,
   Typography,
+  Paper,
 } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { BsCheckSquareFill } from 'react-icons/bs';
@@ -48,73 +48,109 @@ export const StreamingAvailability = ({
   }, [currentMedia, dispatch]);
 
   const tableData = useAppSelector(state => state.streamingAvailability.data);
+  const isTableLoading = useAppSelector(
+    state => state.streamingAvailability.isLoading
+  );
 
   return (
-    <TableContainer
-      component={Paper}
-      sx={{
-        marginBottom: 4,
-        backgroundColor: '#020202',
-        fontFamily: 'Bowlby One SC',
-        minWidth: 350,
-      }}>
-      <Table
-        sx={{
-          maxWidth: 350,
-          '&.MuiTable-root': {
-            color: 'white',
-          },
-        }}
-        aria-label="simple table">
-        <TableHead
-          sx={{
-            '& .MuiTableCell-root': {
-              color: '#FFFF',
-              textAlign: 'center',
-            },
-          }}>
-          <TableRow>
-            <TableCell>Streaming Service</TableCell>
-            <TableCell align="right">Subscription</TableCell>
-            <TableCell align="right">Rent</TableCell>
-            <TableCell align="right">Rental Cost</TableCell>
-            <TableCell align="right">Buy</TableCell>
-            <TableCell align="right">Purchase Cost</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody
-          sx={{
-            '& .MuiTableCell-root': {
-              color: '#FFFF',
-              textAlign: 'center',
-            },
-          }}>
-          {tableData?.map((row: any) => (
-            <TableRow
-              key={row.service}
-              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-              <TableCell component="th" scope="row">
-                {row.service.toUpperCase()}
-              </TableCell>
-              <TableCell align="center">
-                <CheckMark isAvailable={row.subscription} />
-              </TableCell>
-              <TableCell align="center">
-                <CheckMark isAvailable={row.buy} />
-              </TableCell>
-              <TableCell align="center">
-                {row.purchasePrice ? `$${row.purchasePrice}` : 'N/A'}
-              </TableCell>
-              <TableCell align="center">
-                <CheckMark isAvailable={row.rent} />
-              </TableCell>
-              <TableCell align="right">
-                {row.rentalPrice ? `$${row.rentalPrice}` : 'N/A'}
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+    <>
+      {isTableLoading ? (
+        <CircularProgress />
+      ) : (
+        <Box>
+          {tableData ? (
+            <TableContainer
+              component={Paper}
+              sx={{
+                marginBottom: 4,
+                backgroundColor: '#020202',
+                fontFamily: 'Bowlby One SC',
+                minWidth: 350,
+              }}>
+              <Table
+                sx={{
+                  minWidth: 350,
+                  '&.MuiTable-root': {
+                    color: 'white',
+                  },
+                }}
+                aria-label="simple table">
+                <TableHead
+                  sx={{
+                    '& .MuiTableCell-root': {
+                      color: '#FFFF',
+                      textAlign: 'center',
+                    },
+                  }}>
+                  <TableRow>
+                    <TableCell>Streaming Service</TableCell>
+                    <TableCell align="right">Subscription</TableCell>
+                    <TableCell align="right">Rent</TableCell>
+                    <TableCell align="right">Rental Cost</TableCell>
+                    <TableCell align="right">Buy</TableCell>
+                    <TableCell align="right">Purchase Cost</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody
+                  sx={{
+                    '& .MuiTableCell-root': {
+                      color: '#FFFF',
+                      textAlign: 'center',
+                    },
+                  }}>
+                  {tableData?.map((row: any) => (
+                    <TableRow
+                      key={row.service}
+                      sx={{
+                        '&:last-child td, &:last-child th': { border: 0 },
+                      }}>
+                      <TableCell component="th" scope="row">
+                        {row.service.toUpperCase()}
+                      </TableCell>
+                      <TableCell align="center">
+                        <CheckMark isAvailable={row.subscription} />
+                      </TableCell>
+                      <TableCell align="center">
+                        <CheckMark isAvailable={row.buy} />
+                      </TableCell>
+                      <TableCell align="center">
+                        {row.purchasePrice ? `$${row.purchasePrice}` : 'N/A'}
+                      </TableCell>
+                      <TableCell align="center">
+                        <CheckMark isAvailable={row.rent} />
+                      </TableCell>
+                      <TableCell align="right">
+                        {row.rentalPrice ? `$${row.rentalPrice}` : 'N/A'}
+                      </TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          ) : (
+            <Box
+              sx={{
+                minWidth: 350,
+                minHeight: 300,
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+              }}>
+              <Typography
+                variant="h5"
+                sx={{ fontFamily: 'Bowlby One SC', textAlign: 'center' }}>
+                Oh no!
+              </Typography>
+              <Typography
+                variant="h5"
+                sx={{ fontFamily: 'Bowlby One SC', textAlign: 'center' }}>
+                Unable to get availability
+              </Typography>
+            </Box>
+          )}
+        </Box>
+      )}
+    </>
   );
 };
